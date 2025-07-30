43eeae37bf4acd70bd2e148060333e022c51a35a
HerculesDraycon <herculescarros2@gmail.com>
2025-07-30 09:03:33 -0300

    adicionando os packages nos arquivos da q9

--- redes/questao_9/CalculatorClient.java
+++ redes/questao_9/CalculatorClient.java
@@ -1,3 +1,6 @@
+package redes.questao_9;
+
+
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
--- redes/questao_9/CalculatorServer.java
+++ redes/questao_9/CalculatorServer.java
@@ -1,3 +1,5 @@
+package redes.questao_9;
+
 import java.io.*;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
